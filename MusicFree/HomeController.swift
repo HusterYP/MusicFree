3699f4ce5d12c1b04fdc4e1cd301144ae9866894
yuanping <3209605462@qq.com>
2020-01-28 20:42:55 +0800

    init project

---
+++ MusicFree/HomeController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeController.swift
+//  MusicFree
+//
+//  Created by 袁平 on 2020/1/28.
+//  Copyright © 2020 袁平. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MusicFree' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MusicFree
+  pod 'RxSwift'
+  pod 'SwiftyJSON'
+  pod 'Alamofire'
+  pod 'SnapKit'
+end
